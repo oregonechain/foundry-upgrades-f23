6d4cd62cf5b87415b99b7de45c6832503ca7335e
Oregone Chain <oregone@proton.me>
2024-03-11 18:33:44 +0200

    Proxies

---
+++ test/DeployAndUpgradeTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBox} from "../script/DeployBox.s.sol";
+import {UpgradeBox} from "../script/UpgradeBox.s.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+
+contract DeployAndUpgradeTest is Test {
+    DeployBox public deployBox;
+    UpgradeBox public upgradeBox;
+    address public OWNER = address(1);
+
+    address public proxy;
+
+    function setUp() public {
+        deployBox = new DeployBox();
+        upgradeBox = new UpgradeBox();
+        proxy = deployBox.run();
+    }
+
+    function testProxyStartsAsBoxV1() public {
+        vm.expectRevert();
+        BoxV2(proxy).setNumber(7);
+    }
+
+    function testUpgrades() public {
+        BoxV2 box2 = new BoxV2();
+
+        upgradeBox.upgradeBox(proxy, address(box2));
+
+        uint256 expectedValue = 2;
+        assertEq(expectedValue, BoxV2(proxy).version());
+
+        BoxV2(proxy).setNumber(7);
+        assertEq(7, BoxV2(proxy).getNumber());
+    }
+}
